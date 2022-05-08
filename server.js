815ae3f331d2f615fa90646409d6b32f3f036bfa
Mridul Tiwari <92802892+MridulTi@users.noreply.github.com>
2022-05-08 10:02:27 +0530

    updated server.js

--- server.js
+++ server.js
@@ -9,20 +9,6 @@ const mongoose=require('mongoose')
 //mongodb+srv://mridultiwar:mridultiwari@cluster0.ub68a.mongodb.net/Search BAckend?retryWrites=true&w=majority
 //app.use(cors)
 app.use("/static", express.static('./static/'))
-let savage=[
-    {		
-        	id:2,
-        	content:{
-        		"Name":"Katsuki Bakugo",
-        		"Description": "Izuku 'Deku' Midoriya is the main protagonist of the manga and anime My Hero Academia and is in Class 1-A at U.A. High School.",
-            	"Birthday": "July 15,2010(Cancer)",
-            	"Alternative_Name": "Kacchan",
-            	"Backstory": "In a world where most humans on the planet develop superpowers soon after they're born, a boy named Izuku Midoriya was one of the few who never developed any powers. As a child, Izuku dreamed to become a superhero like his idol, All Might, after he developed his power, or 'Quirk'. As such, he was devastated once he learned that he'd never get a quirk. His lack of a Quirk made him a prime target for bullying, but Izuku still wanted to be a hero, and planned to go to the prestigious U.A. hero academy. Ten months before the entrance exams for U.A. started, Izuku came face to face with All Might after he was saved from a villain by him. Izuku asked All Might if it was possible for someone without a quirk to be a hero, but he responded by telling him that he needed realistic dreams. Soon after, the same villain from before ended up breaking free and attacking one of Izuku classmates. Despite not having a quirk, Izuku still tried to save his classmate. While he didn't stop the villain, he did give All Might enough time to defeat them himself. After this, All Might decided that Izuku had the potential to become a hero, with his help. He decided that he would grant Izuku his own Quirk to help him achieve his dreams. After ten months of training, All Might passed his Quirk, One For All, down to Izuku. Izuku then managed to pass the entrance exam, and officially enrolled in U.A, taking on the Hero name 'Deku",
-            	"Anime/Manga": "Boku No Hero Academia"
-        	}
-            
-    },
-]
 app.set('view engine','ejs');
 const uri="mongodb+srv://mridultiwar:mridultiwari@cluster0.ub68a.mongodb.net/SearchBAckend?retryWrites=true&w=majority";
 mongoose.connect(uri);
@@ -76,7 +62,7 @@ app.post('/posts',function(req,res,next){
     //main()
   //  res.render('info.ejs',{info:"name"})
 //})
-async function main(){
+/*async function main(){
         try{
             await client.connect();
             await findOnebyName(client,"Ochaco Uraraka");
@@ -119,7 +105,7 @@ async function main(){
 }
     
 
-
+*/
 // // sav.save().then(result=>{
 // //     console.log('sav Saved');
 // mongoose.connection.close();
